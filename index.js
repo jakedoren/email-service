1dfc422d88bc6551e3d8e4f83e4c5e910b87d81a
Jacob Doren <jakedoren@gmail.com>
2021-07-09 17:28:22 -0400

    housekeeping

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const PORT = process.env.PORT || 8080;
 
 require('dotenv').config()
 
+// Middleware
+app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use('/', require('./router/email'))
 
 app.listen(PORT, () => {
