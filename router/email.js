a08b1a46e53b47aeb074c065bd61c8b5661db5c9
Jacob Doren <jakedoren@gmail.com>
2022-01-01 21:58:09 -0500

    heroku migration

--- router/email.js
+++ router/email.js
@@ -10,29 +10,27 @@ const transporter = nodemailer.createTransport({
     }
 })
 
-router.post('/', (req, res) => {
-    
-    const { name, email, message } = req.body;
+router.get('/', (req, res) => {
+    res.status(400).json({success: "Welcome to the contact service"})
+})
 
+router.post('/', (req, res) => {
+    const { name, email, message } = req.body;
     if(!name || !email || !message) {
         return res.status(400).json({
             errorMessage: "Please enter in all required fields"
         })
     }
-
     const checkedEmail = validator.validate(email)
-
     if(!checkedEmail) {
         return res.status(400).json({errorMessage: "Please enter in a valid email address"})
     }
-
     const mailOptions = {
         from: email,
         to: process.env.EMAIL,
         subject: `Web Contact Form Message from: ${email}`,
         text: `${name} says "${message}" \n \n \nContact them at ${email}`
     }
-
     transporter.sendMail(mailOptions, (err, info) => {
         if(err) {
             console.log(err)
@@ -44,7 +42,6 @@ router.post('/', (req, res) => {
             })
         }
     })
-    
 })
 
 module.exports = router;
